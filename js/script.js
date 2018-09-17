6e508b2ec2650ec19fe3d698a2c842e7a67904b0
Raine <raineldn@gmail.com>
2018-09-17 19:00:29 +0100

    Added show 'x' amount of students on page
    Used jQuery.

---
+++ js/script.js
@@ -0,0 +1,30 @@
+/******************************************
+Treehouse Techdegree:
+FSJS project 2 - List Filter and Pagination
+******************************************/
+
+// Add variables that store DOM elements you will need to reference and/or manipulate
+
+const numberOfStudents = $('.page .student-item').length;
+const limitPerPage = 10;
+
+
+// Create a function to hide all of the items in the list excpet for the ten you want to show
+// Tip: Keep in mind that with a list of 54 studetns, the last page will only display four
+
+$('.page .student-item:gt(' + (limitPerPage - 1) +')').hide();
+
+
+// Create and append the pagination links - Creating a function that can do this is a good approach
+
+
+
+
+// Add functionality to the pagination buttons so that they show and hide the correct items
+// Tip: If you created a function above to show/hide list items, it could be helpful here
+
+
+
+
+
+
